1531d18cbbf2de59b5eeeea24f2cca442a6cf5ce
jyoko <jyoko@users.noreply.github.com>
2015-08-17 19:59:48 -0400

    test

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -1,3 +1,101 @@
 var parseJSON = function(json) {
-  // your code goes here
+  // The easy way:
+  // return eval('('+json+')');
+
+  // The hard way:
+
+  // escape char obj
+  var esc = {};
+  // at is current position #
+  var at;
+  // chr is current character
+  var chr;
+  // result is obj being built
+  var result;
+
+  var error = function(e) {
+    throw new SyntaxError(e+' at '+at+' in '+json), 'parseJSON');
+  };
+
+  var string = function() {
+
+  };
+
+  var array = function() {
+    
+  };
+
+  var number = function() {
+    var result = chr;
+    next();
+    while (chr.match(/\d/)) {
+      result = result + chr;
+      next();
+    }
+    return +result;
+  };
+
+  var object = function() {
+
+  };
+
+  var word = function() {
+    switch(chr) {
+      case 't':
+        next('t');
+        next('r');
+        next('u');
+        next('e');
+        return true;
+      case 'f':
+        next('f');
+        next('a');
+        next('l');
+        next('s');
+        next('e');
+        return false;
+      case 'n':
+        next('n');
+        next('u');
+        next('l');
+        next('l');
+        return null;
+    }
+    error('Unexpected "'+chr+'"');
+  };
+  
+  // move to next char
+  var next = function(c) {
+    if (c && c!==chr) {
+      error('Expected "'+c+'"');
+    }
+    at+=1;
+    chr = json.charAt(at);
+    return chr;
+  };
+
+  // this is what loops
+  var value = function() {
+    while ( chr.match(/\s/) ) {
+      next();
+    }
+    if (chr==='{') {
+      return object();
+    }
+    if (chr==='[') {
+      return array();
+    }
+    if (chr==='-' || chr.match(/\d/) ) {
+      return number();
+    }
+    if (chr==='"') {
+      return string();
+    }
+    return word();
+  };
+
+  at = -1;
+  next();
+  result = value();
+  return result;
 };
